296b7eb5e143de926b74a09167d98836f0353ea4
Juliana Sousa Nogueira <juliana.snogueira@ceub.edu.br>
2025-03-06 18:56:24 -0300

    Projeto criado

---
+++ src/main/java/com/juliana/cursomcj/CursomcjApplication.java
@@ -0,0 +1,13 @@
+package com.juliana.cursomcj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juliana/cursomcj/CursomcjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliana.cursomcj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
